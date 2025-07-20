e4af1e3e5d6eb2220f489bed81180e4964341d81
darqus.ru <darqus.ru@gmail.com>
2025-07-20 21:30:23 +0000

    update avatar

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -5,7 +5,11 @@
     </v-btn>
     <div v-else class="d-flex align-center">
       <v-avatar size="36" class="mr-3">
-        <img :src="userStore.user?.photoURL ?? ''" alt="avatar">
+        <v-img
+          :src="userStore.user?.photoURL ?? ''"
+          alt="User Avatar"
+          cover
+        ></v-img>
       </v-avatar>
       <span>{{ userStore.user?.displayName }}</span>
       <v-btn @click="userStore.logout" class="ml-4" variant="outlined">
