a0bf19b93cc0932b4cd784830809430f8bb4eef5
darqus.ru <darqus.ru@gmail.com>
2025-07-20 21:22:03 +0000

    add alias @

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vuetify from 'vite-plugin-vuetify'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vuetify({ autoImport: true }),
   ],
-  server: {
-    port: 3000
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   }
 })
